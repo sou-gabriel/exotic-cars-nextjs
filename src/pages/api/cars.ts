26cd22f10774dad06ec30fe906672b706b5bfbd7
Gabriel Ramos <dev.gabrielramos@gmail.com>
2022-02-07 14:18:40 -0300

    Create cars api

---
+++ src/pages/api/cars.ts
@@ -0,0 +1,14 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import fs from 'fs'
+import path from 'path'
+
+const handler = (request: NextApiRequest, response: NextApiResponse) => {
+  if (request.method === 'GET') {
+    const filePath = path.resolve(process.cwd(), 'database', 'cars.json')
+    const data = fs.readFileSync(filePath, 'utf-8')
+
+    return response.status(200).json(data)
+  }
+}
+
+export default handler
