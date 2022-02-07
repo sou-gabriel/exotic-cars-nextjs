210c283b0ce3421a80eda72b0dacb38be80aebb1
Gabriel Ramos <dev.gabrielramos@gmail.com>
2022-02-07 14:16:49 -0300

    Create layout component

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from 'react'
+import { ThemeProvider } from 'styled-components'
+
+import { Header } from '../Header'
+
+import { theme } from 'styles/theme'
+import { GlobalStyle } from 'styles/global'
+import * as S from './styles'
+
+interface ILayout {
+  children: ReactNode | ReactNode[]
+}
+
+export const Layout = ({ children }: ILayout) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Header />
+      <main>
+        <S.Content>{children}</S.Content>
+      </main>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
---
+++ src/components/Layout/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Content = styled.div`
+  max-width: 1220px;
+
+  margin: 0 auto;
+  padding: 0 0.5rem;
+`
