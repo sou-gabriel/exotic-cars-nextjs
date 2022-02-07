c3ea1e0b99fd0c27d4d276ac2e4176733a2b30a4
Gabriel Ramos <dev.gabrielramos@gmail.com>
2022-02-07 14:16:03 -0300

    Create initials colors theme

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,14 @@
+import 'styled-components'
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      shape: string
+      title: string
+      text: string
+      background: string
+      bulletBackground: string
+      purple: string
+    }
+  }
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'styled-components'
+
+export const theme: DefaultTheme = {
+  colors: {
+    shape: '#fff',
+    title: '#313136',
+    text: '#656469',
+    background: '#d8d7d7',
+    bulletBackground: '#c8c8ca',
+    purple: '#7b89f4',
+  },
+}
