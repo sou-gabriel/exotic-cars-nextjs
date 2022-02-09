1bfc064c15456b1734f2c30103a83db9cb58a904
Gabriel Ramos <dev.gabrielramos@gmail.com>
2022-02-09 10:50:02 -0300

    Refactor cars API

--- src/pages/api/cars.ts
+++ src/pages/api/cars.ts
@@ -3,8 +3,15 @@ import fs from 'fs'
 import path from 'path'
 
 const handler = (request: NextApiRequest, response: NextApiResponse) => {
-  if (request.method === 'GET') {
-    const filePath = path.resolve(process.cwd(), 'database', 'cars.json')
+  const isAGetMethod = request.method === 'GET'
+
+  if (isAGetMethod) {
+    const currentWorkingDirectory = process.cwd()
+    const filePath = path.resolve(
+      currentWorkingDirectory,
+      'database',
+      'cars.json'
+    )
     const data = fs.readFileSync(filePath, 'utf-8')
 
     return response.status(200).json(data)
